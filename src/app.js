5c556fdc16f09a5c58d49ba59dac68d9a7976604
surajsahu665 <62028463+surajsahu665@users.noreply.github.com>
2021-01-12 22:05:12 +0530

    ee

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT || 3000;
 // console.log(__dirname);  //inbuild fun
 // console.log(__filename);  //inbuild fun
 //app.use(express.static(path.join(__dirname, '../public')))  //inbuild method   it is use to add middleware
@@ -51,20 +51,20 @@ app.get('/weather', (req, res) => {
 });
 
 
-// // WITH ARRAY   USING API
-// app.get('/weather', (req, res) => {
-//     console.log("get api");
-//     const url = 'http://api.weatherstack.com/current?access_key=92aae5f123982b43a5a6244518853471&query=bhilai';
-//     console.log(weather);
-//     res.send([{
-//         lat: '81.343434',
-//         lon: '41.8746753'
-//     },
-//     { Name: 'suraj sahu' }
-//     ]);
-// });
+// WITH ARRAY   USING API
+app.get('/weather', (req, res) => {
+    console.log("get api");
+    const url = 'http://api.weatherstack.com/current?access_key=92aae5f123982b43a5a6244518853471&query=bhilai';
+    console.log(weather);
+    res.send([{
+        lat: '81.343434',
+        lon: '41.8746753'
+    },
+    { Name: 'suraj sahu' }
+    ]);
+});
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server is running on port 3000");
 })
